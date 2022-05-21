79679f40fa5edf20660fefb3f667a0d99edb6fc7
anastasiya0kvardakova <90642635+anastasiya0kvardakova@users.noreply.github.com>
2022-05-21 06:27:55 +0300

    Update admin.php

--- admin.php
+++ admin.php
@@ -221,7 +221,7 @@ if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW'])) {
         ?>
     </table>
     <?php if (!empty($_POST['edit'])) {
-        include('adminchange.php');
+        include('edit.php');
     } ?>
 </body>
 
