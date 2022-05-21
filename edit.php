47e504991e56df7b5d571453636ecdd116f95bd3
anastasiya0kvardakova <90642635+anastasiya0kvardakova@users.noreply.github.com>
2022-05-21 06:31:35 +0300

    Update edit.php

--- edit.php
+++ edit.php
@@ -66,9 +66,7 @@
                 </select>
             </div>
             <div class="input-group">
-                <textarea class="form-control" placeholder="Расскажите о себе..." name="bio" <?php if ($errors['bio']) {
-                                                                                                    print 'class="error"';
-                                                                                                } ?>><?php print $values['bio']; ?></textarea>
+                <textarea class="form-control" placeholder="Расскажите о себе..." name="bio"></textarea>
             </div>
             <div class="form-check" id="policy">
                 <input class="form-check-input" type="checkbox" value="y" id="policy" name="policy" checked />
